8e0412e0cd77f86a69250951728955a80676e1ec
Guillaume Giboury <ggiboury@k0r3p14.42mulhouse.fr>
2025-05-29 18:42:53 +0200

    Pre merge

--- srcs/services/fastify/src/chat.ts
+++ srcs/services/fastify/src/chat.ts
@@ -2,7 +2,7 @@ let _ws = null;
 
 async function init() {
     // Se connecter a la socket
-    await swapLang("fr");
+    await swapLang("en");
     updateContent();
     _ws = new WebSocket(`wss://${window.location.host}/ws?username=${_username}`);
     console.log(_username);
