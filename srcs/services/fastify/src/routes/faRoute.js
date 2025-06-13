99037d48fe076dadb7c7a3f7809cc7894600cec7
RobinM258 <darovi.martinez@gmail.com>
2025-06-13 16:50:54 +0200

    ajout du plugin et la colonne 2fa dans le server

--- srcs/services/fastify/src/routes/faRoute.js
+++ srcs/services/fastify/src/routes/faRoute.js
@@ -1,5 +1,5 @@
 const clientId = 'u-s4t2ud-35286bd3f00c601d663caf2387975ae7802bc026283c7cbe917483ea607f3f8e';
-const redirectUri = 'https://k0r3p14.42mulhouse.fr:3000/callback';
+const redirectUri = 'https://k0r1p8.42mulhouse.fr:3000/callback';
 const clientSecret = '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';
 
 let username = null;
@@ -24,7 +24,7 @@ async function faRoute(fastify, options) {
   }
   fastify.get('/2fa', async (req, reply) => {
     const client_UID = 'u-s4t2ud-35286bd3f00c601d663caf2387975ae7802bc026283c7cbe917483ea607f3f8e';
-    const redirect_URI = 'https://k0r3p14.42mulhouse.fr:3000/callback'
+    const redirect_URI = 'https://k0r1p8.42mulhouse.fr:3000/callback'
     const authUrl = `https://api.intra.42.fr/oauth/authorize?client_id=${client_UID}&redirect_uri=${redirect_URI}&response_type=code`;
 
     const token = req.cookies.auth_token;
